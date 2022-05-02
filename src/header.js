5460e47dc5b4ad875be514fe3830aa2d29a1c0c1
david-soto-m <davidsotomarchena@protonmail.com>
2022-05-02 12:59:48 +0200

    header + css

--- src/header.js
+++ src/header.js
@@ -4,11 +4,11 @@ module.exports = {
     view: function(vnode) {
         return [
             m("nav.header",[
+                m("div.icon", "Icon here"),
                 m("div.components", [
-                    m(m.route.Link, {href: "/undone"},"Inicio"),
-                    m(m.route.Link, {href: "/undone"},"Servicios"),
-                    m(m.route.Link, {href: "/undone"},"Sobre nosotros"),
-                    // m(m.route.Link, {href: "/undone"},"nothing"),
+                    m(m.route.Link, {href: "/"},"Inicio"),
+                    m(m.route.Link, {href: "/services"},"Servicios"),
+                    m(m.route.Link, {href: "/about"},"Sobre nosotros"),
                 ])
             ]),
             m("div.main", vnode.children),
