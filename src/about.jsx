a95881530105c3b79a18013b70bb56636cfde30e
david-soto-m <davidsotomarchena@protonmail.com>
2022-05-19 11:57:04 +0200

    alicia change

--- src/about.jsx
+++ src/about.jsx
@@ -1,16 +1,15 @@
-module.exports = {
-    view: ()=>(
-        <div class="text">
-            <h1>Sobre nosotros</h1>
-            <p>
-Somos un grupo de alumnos de máster de la universidad de Sevilla que queremos
-traer tecnología puntera en la industria al campo. Queremos reducir los costes y
-el impacto ambiental de la agricultura y empoderar a los agricultores en  sus
-negociaciones de formas similares a como se hace ya en muchas industrias.
-            </p>
-        </div>
-    )
-};
+module.exports = { view: ()=>(
+    <div class="text">
+        <h1>Sobre nosotros</h1>
+        <p>
+Somos un grupo de alumnos de Máster de la Universidad de Sevilla que queremos
+trasladar tecnología puntera de la industria al campo. Además queremos reducir
+los costes y el impacto ambiental de la agricultura y empoderar a los
+agricultores en sus negociaciones de forma similar a como ya se hace en muchas
+industrias de diversos sectores
+        </p>
+    </div>
+)};
 
 
 
